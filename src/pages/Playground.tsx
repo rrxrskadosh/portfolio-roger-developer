285351152113f384cc3830c47e978bb40dc06c29
rrxrskadosh <kadoshrr2019@gmail.com>
2025-12-04 12:18:27 -0500

    Deleted useView in Playground

--- src/pages/Playground.tsx
+++ src/pages/Playground.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useRef, useState } from "react";
-import { motion, useScroll, useTransform, useInView } from "framer-motion";
+import { motion, useScroll, useTransform } from "framer-motion";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import SplitType from "split-type";
